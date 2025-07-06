9aeebcd49c574af6b7c3e9c316656c3692fa048e
Abhishek chaurasiya <166469637+Imabhishek32@users.noreply.github.com>
2025-07-06 14:22:34 +0530

    Add files via upload

---
+++ 20250510132041_AddProductToDb.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUDApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "products");
+        }
+    }
+}
---
+++ ProductController.cs
@@ -0,0 +1,96 @@
+﻿using CRUDApp.Data;
+using CRUDApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace CRUDApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> products = _context.products.ToList();
+            return View(products);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([Bind("Name , Price, Description, Stock")] Product modal)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.products.Add(modal);
+                _context.SaveChanges();
+                TempData["Notification"] = "Product Create Successfully";
+                TempData["NotificationType"] = "success";
+                return RedirectToAction("Index");
+            }
+            return View(modal);
+        }
+        [HttpGet]
+
+        public IActionResult Edit(int id)
+        {
+            var product = _context.products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+        [HttpPost]
+        public IActionResult Edit([Bind("Id, Name , Price, Description, Stock")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.products.Update(product);
+                _context.SaveChanges();
+                TempData["Notification"] = "Product Updated Successfully";
+                TempData["NotificationType"] = "success";
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var product = _context.products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+        public IActionResult DeleteConfirm(int id)
+        {
+            var product = _context.products.Find(id);
+            if (product != null)
+            {
+                _context.products.Remove(product);
+                _context.SaveChanges();
+                TempData["Notification"] = "Product Deleted Successfully";
+                TempData["NotificationType"] = "success";
+            }
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
